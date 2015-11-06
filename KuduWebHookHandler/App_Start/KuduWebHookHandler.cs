1616eea2bfee63fa29dcd075c9cf937b0ec1d863
davidebbo <david.ebbo@microsoft.com>
2015-11-06 10:02:40 -0800

    Add Kudu handler

---
+++ KuduWebHookHandler/App_Start/KuduWebHookHandler.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNet.WebHooks;
+
+namespace KuduWebHookHandler.App_Start
+{
+    public class KuduWebHookHandler : WebHookHandler
+    {
+        public KuduWebHookHandler()
+        {
+            Receiver = "kudu";
+        }
+
+        public override Task ExecuteAsync(string generator, WebHookHandlerContext context)
+        {
+            // Convert to POCO type
+            KuduNotification notification = context.GetDataOrDefault<KuduNotification>();
+
+            // Get the notification message
+            string message = notification.Message;
+
+            // Get the notification author
+            string author = notification.Author;
+
+            return Task.FromResult(true);
+        }
+    }
+}
